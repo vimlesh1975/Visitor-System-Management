c80ff1323079d072bf8065f83cbfe6e6572dc761
vimlesh1975 <vimlesh1975@hotmail.com>
2022-06-23 17:00:42 +0530

    Add project files.

---
+++ WebCam/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgv1 = New System.Windows.Forms.DataGridView()
+        Me.VisitorImage = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.VisitorName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Age = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Address = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VisitordateAndTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.OutTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.cmdno = New System.Windows.Forms.Button()
+        Me.cmdok = New System.Windows.Forms.Button()
+        Me.pbcapture = New PictureBoxAbouAmmar.صورة_دائرة()
+        Me.pbcaptureimage = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cmdopen = New System.Windows.Forms.Button()
+        Me.cmdSetOutTime = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSeacrh = New System.Windows.Forms.TextBox()
+        Me.cmdClearSearch = New System.Windows.Forms.Button()
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbcapture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbcaptureimage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgv1
+        '
+        Me.dgv1.AllowUserToAddRows = False
+        Me.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.VisitorImage, Me.VisitorName, Me.Age, Me.Address, Me.VisitordateAndTime, Me.OutTime})
+        Me.dgv1.Location = New System.Drawing.Point(12, 12)
+        Me.dgv1.Name = "dgv1"
+        Me.dgv1.RowTemplate.Height = 50
+        Me.dgv1.Size = New System.Drawing.Size(650, 313)
+        Me.dgv1.TabIndex = 4
+        '
+        'VisitorImage
+        '
+        Me.VisitorImage.HeaderText = "Image"
+        Me.VisitorImage.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch
+        Me.VisitorImage.Name = "VisitorImage"
+        '
+        'VisitorName
+        '
+        Me.VisitorName.HeaderText = "Name"
+        Me.VisitorName.Name = "VisitorName"
+        '
+        'Age
+        '
+        Me.Age.HeaderText = "Age"
+        Me.Age.Name = "Age"
+        '
+        'Address
+        '
+        Me.Address.HeaderText = "Address"
+        Me.Address.Name = "Address"
+        '
+        'VisitordateAndTime
+        '
+        Me.VisitordateAndTime.HeaderText = "In Time"
+        Me.VisitordateAndTime.Name = "VisitordateAndTime"
+        '
+        'OutTime
+        '
+        Me.OutTime.HeaderText = "Out Time"
+        Me.OutTime.Name = "OutTime"
+        '
+        'cmdno
+        '
+        Me.cmdno.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdno.Image = Global.WebCam.My.Resources.Resources.Actions_edit_delete_icon
+        Me.cmdno.Location = New System.Drawing.Point(958, 347)
+        Me.cmdno.Name = "cmdno"
+        Me.cmdno.Size = New System.Drawing.Size(32, 24)
+        Me.cmdno.TabIndex = 3
+        Me.cmdno.UseVisualStyleBackColor = True
+        '
+        'cmdok
+        '
+        Me.cmdok.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdok.Image = Global.WebCam.My.Resources.Resources.Ok_icon__1_1
+        Me.cmdok.Location = New System.Drawing.Point(920, 347)
+        Me.cmdok.Name = "cmdok"
+        Me.cmdok.Size = New System.Drawing.Size(32, 24)
+        Me.cmdok.TabIndex = 2
+        Me.cmdok.UseVisualStyleBackColor = True
+        '
+        'pbcapture
+        '
+        Me.pbcapture.BackColor = System.Drawing.Color.DarkGray
+        Me.pbcapture.BackgroundImage = Global.WebCam.My.Resources.Resources.Image_Capture_icon
+        Me.pbcapture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbcapture.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pbcapture.Location = New System.Drawing.Point(927, 331)
+        Me.pbcapture.Name = "pbcapture"
+        Me.pbcapture.Size = New System.Drawing.Size(52, 50)
+        Me.pbcapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbcapture.TabIndex = 1
+        Me.pbcapture.TabStop = False
+        '
+        'pbcaptureimage
+        '
+        Me.pbcaptureimage.Location = New System.Drawing.Point(702, 12)
+        Me.pbcaptureimage.Name = "pbcaptureimage"
+        Me.pbcaptureimage.Size = New System.Drawing.Size(487, 313)
+        Me.pbcaptureimage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbcaptureimage.TabIndex = 0
+        Me.pbcaptureimage.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(99, 383)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cmdopen
+        '
+        Me.cmdopen.Location = New System.Drawing.Point(206, 383)
+        Me.cmdopen.Name = "cmdopen"
+        Me.cmdopen.Size = New System.Drawing.Size(75, 23)
+        Me.cmdopen.TabIndex = 6
+        Me.cmdopen.Text = "Open"
+        Me.cmdopen.UseVisualStyleBackColor = True
+        '
+        'cmdSetOutTime
+        '
+        Me.cmdSetOutTime.Location = New System.Drawing.Point(587, 330)
+        Me.cmdSetOutTime.Name = "cmdSetOutTime"
+        Me.cmdSetOutTime.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSetOutTime.TabIndex = 7
+        Me.cmdSetOutTime.Text = "Set Out Time"
+        Me.cmdSetOutTime.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 332)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Search"
+        '
+        'txtSeacrh
+        '
+        Me.txtSeacrh.Location = New System.Drawing.Point(60, 332)
+        Me.txtSeacrh.Name = "txtSeacrh"
+        Me.txtSeacrh.Size = New System.Drawing.Size(140, 20)
+        Me.txtSeacrh.TabIndex = 9
+        '
+        'cmdClearSearch
+        '
+        Me.cmdClearSearch.Location = New System.Drawing.Point(207, 328)
+        Me.cmdClearSearch.Name = "cmdClearSearch"
+        Me.cmdClearSearch.Size = New System.Drawing.Size(84, 23)
+        Me.cmdClearSearch.TabIndex = 10
+        Me.cmdClearSearch.Text = "Clear Serach"
+        Me.cmdClearSearch.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1209, 583)
+        Me.Controls.Add(Me.cmdClearSearch)
+        Me.Controls.Add(Me.txtSeacrh)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmdSetOutTime)
+        Me.Controls.Add(Me.cmdopen)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.dgv1)
+        Me.Controls.Add(Me.cmdno)
+        Me.Controls.Add(Me.cmdok)
+        Me.Controls.Add(Me.pbcapture)
+        Me.Controls.Add(Me.pbcaptureimage)
+        Me.Name = "Form1"
+        Me.Text = "WebCam"
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbcapture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbcaptureimage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents pbcaptureimage As System.Windows.Forms.PictureBox
+    Friend WithEvents pbcapture As PictureBoxAbouAmmar.صورة_دائرة
+    Friend WithEvents cmdok As System.Windows.Forms.Button
+    Friend WithEvents cmdno As System.Windows.Forms.Button
+    Friend WithEvents dgv1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents cmdopen As Button
+    Friend WithEvents VisitorImage As DataGridViewImageColumn
+    Friend WithEvents VisitorName As DataGridViewTextBoxColumn
+    Friend WithEvents Age As DataGridViewTextBoxColumn
+    Friend WithEvents Address As DataGridViewTextBoxColumn
+    Friend WithEvents VisitordateAndTime As DataGridViewTextBoxColumn
+    Friend WithEvents OutTime As DataGridViewTextBoxColumn
+    Friend WithEvents cmdSetOutTime As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSeacrh As TextBox
+    Friend WithEvents cmdClearSearch As Button
+End Class
---
+++ WebCam/Form1.vb
@@ -0,0 +1,255 @@
+﻿Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
+Imports System.IO
+Public Class Form1
+    Dim CAMARA As VideoCaptureDevice
+    Dim BMP As Bitmap
+    Dim Cap As String = "Capture"
+    Dim osd2 As New SaveFileDialog
+    Dim cameraStarted = False
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Try
+            If cameraStarted = True Then
+                CAMARA.Stop()
+            End If
+            ' PictureBox2.Image = Nothing
+        Catch ex As Exception
+            Me.Dispose()
+            'PictureBox2.Image = Nothing
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmdno.Visible = False
+        cmdok.Visible = False
+        dgv1.Rows.Add(1)
+        Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+
+        If CAMARAS.ShowDialog() = DialogResult.OK Then
+            CAMARA = CAMARAS.VideoDevice
+
+            AddHandler CAMARA.NewFrame, New NewFrameEventHandler(AddressOf CAPTURAR)
+            CAMARA.Start()
+            cameraStarted = True
+        Else
+
+            Me.Close()
+
+        End If
+    End Sub
+    Private Sub CAPTURAR(sender As Object, eventArgs As NewFrameEventArgs)
+
+        BMP = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+        pbcaptureimage.Image = BMP ' DirectCast(eventArgs.Frame.Clone(), Bitmap)
+
+
+    End Sub
+    Private Sub pbcapture_Click(sender As Object, e As EventArgs) Handles pbcapture.Click
+        If Cap = "Capture" Then
+            cmdno.Visible = True
+            cmdok.Visible = True
+            pbcapture.Visible = False
+            CAMARA.Stop()
+            Cap = "Start"
+        ElseIf Cap = "Start" Then
+            Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+            CAMARA.Start()
+            cmdno.Visible = False
+            cmdok.Visible = False
+            Cap = "Capture"
+        End If
+    End Sub
+
+    Private Sub cmdno_Click(sender As Object, e As EventArgs) Handles cmdno.Click
+        Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+        CAMARA.Start()
+        cmdno.Visible = False
+        cmdok.Visible = False
+        pbcapture.Visible = True
+        Cap = "Capture"
+    End Sub
+
+    Private Sub cmdok_Click(sender As Object, e As EventArgs) Handles cmdok.Click
+        'Dim SD As New SaveFileDialog
+        'SD.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+        'SD.FileName = "Visual Capture " & Date.Now.ToString("MM/dd/yy/HH/mm/ss")
+        'SD.SupportMultiDottedExtensions = True
+        'SD.AddExtension = True
+        'SD.Filter = "PNG File|*.png"
+        'If SD.ShowDialog() = DialogResult.OK Then
+        '    Try
+        '        pbcaptureimage.Image.Save(SD.FileName, Imaging.ImageFormat.Png)
+        '        Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+        '        CAMARA.Start()
+        '        cmdno.Visible = False
+        '        cmdok.Visible = False
+        '        pbcapture.Visible = True
+        '        Cap = "Capture"
+        '        dgv1.CurrentRow.Cells(3).Value = pbcaptureimage.Image
+        '        savefile()
+
+
+        '    Catch ex As Exception
+
+        '    End Try
+        'Else
+        '    Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+        '    CAMARA.Start()
+        '    cmdno.Visible = False
+        '    cmdok.Visible = False
+        '    pbcapture.Visible = True
+        '    Cap = "Capture"
+        'End If 
+        dgv1.CurrentRow.Cells("VisitorImage").Value = pbcaptureimage.Image
+        dgv1.CurrentRow.Cells("VisitorName").Value = "Vimlesh Kumar" & dgv1.CurrentRow.Index
+        dgv1.CurrentRow.Cells("Age").Value = 30 + dgv1.CurrentRow.Index * 2
+        dgv1.CurrentRow.Cells("Address").Value = "Pratapgad 505, kamothe, Navi Mumbai, mh 410209"
+        dgv1.CurrentRow.Cells("VisitordateAndTime").Value = Date.Now
+
+
+        Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+        CAMARA.Start()
+        cmdno.Visible = False
+        cmdok.Visible = False
+        pbcapture.Visible = True
+        Cap = "Capture"
+        If dgv1.CurrentRow.Index = 0 Then
+            dgv1.Rows.Add(1)
+            GoTo 50
+        End If
+        If dgv1.Rows(dgv1.RowCount - 1).Cells(3).Value IsNot Nothing Then
+            dgv1.Rows.Add(1)
+        End If
+50:
+        Dim aa = dgv1.RowCount - 1
+        'dgv1.CurrentCell = dgv1(0, aa - 1)
+        For i = 0 To dgv1.RowCount - 1
+            If dgv1.Rows(i).Cells(3).Value Is Nothing Then
+                dgv1.CurrentCell = dgv1(0, i)
+                Exit For
+            End If
+        Next
+        'savefile()
+
+    End Sub
+
+    Sub savefile()
+        'On Error Resume Next
+        osd2.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        osd2.InitialDirectory = "d:\_camera\"
+
+        osd2.FileName = Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetDayName((Date.Today).DayOfWeek) & "_" & (Date.Today).Day & "_" & (Date.Today).Month & "_" & (Date.Today).Year & "_" & (TimeOfDay.Hour + 1) & "HRS"
+        'If (osd2.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+        'Using sw As StreamWriter = New StreamWriter(osd2.FileName)
+        Using sw As StreamWriter = New StreamWriter("aa.txt")
+
+            If dgv1.Rows.Count = 0 Then
+                sw.Write("")
+            Else
+                'Loop through and add list to the file.
+                Dim f As Integer = 0
+                Do Until f = dgv1.Rows.Count - 1
+                    'If dgv1.Rows(f).Cells("x").Value = False Then dgv1.Rows(f).Cells("x").Value = "0"
+                    'If dgv1.Rows(f).Cells("Column11").Value = False Then dgv1.Rows(f).Cells("Column11").Value = "0"
+                    'If dgv1.Rows(f).Cells("Conversion").Value = False Then dgv1.Rows(f).Cells("Conversion").Value = "0"
+                    'If dgv1.Rows(f).Cells("BackIn").Value = False Then dgv1.Rows(f).Cells("BackIn").Value = "0"
+
+                    sw.WriteLine(dgv1.Rows(f).Cells("VisitorName").Value & Chr(2) & dgv1.Rows(f).Cells("Age").Value & Chr(2) & dgv1.Rows(f).Cells("Address").Value & Chr(2) & ImageToBase64(dgv1.Rows(f).Cells("VisitorImage").Value, Imaging.ImageFormat.Png) & Chr(2) & dgv1.Rows(f).Cells("VisitordateAndTime").Value & Chr(2) & dgv1.Rows(f).Cells("OutTime").Value)
+                    f = f + 1
+                Loop
+            End If
+            sw.Close()
+        End Using
+        'Me.lblplalistname.Text = "playlist=  " & osd2.FileName
+        'SaveToolStripMenuItem.Enabled = True
+        'End If
+    End Sub
+
+    Private Sub dgv1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv1.CellContentClick
+        If e.ColumnIndex = 0 Then
+            Form2.Show()
+            Form2.pbcaptureimage.Image = dgv1.CurrentRow.Cells("VisitorImage").Value
+        End If
+    End Sub
+
+    Private Sub dgv1_DefaultValuesNeeded(sender As Object, e As DataGridViewRowEventArgs) Handles dgv1.DefaultValuesNeeded
+        e.Row.Cells(0).Value = "Vimlesh Kumar"
+    End Sub
+
+    Public Function ImageToBase64(ByVal image As Image, ByVal format As System.Drawing.Imaging.ImageFormat) As String
+        Using ms As New MemoryStream()
+            ' Convert Image to byte[]
+            image.Save(ms, format)
+            Dim imageBytes As Byte() = ms.ToArray() ' Convert byte[] to Base64 String
+            Dim base64String As String = Convert.ToBase64String(imageBytes)
+            Return base64String
+        End Using
+    End Function
+
+    Public Function Base64ToImage(ByVal base64String As String) As Image
+        ' Convert Base64 String to byte[]
+        Dim imageBytes As Byte() = Convert.FromBase64String(base64String)
+        Dim ms As New MemoryStream(imageBytes, 0, imageBytes.Length)
+
+        ' Convert byte[] to Image
+        ms.Write(imageBytes, 0, imageBytes.Length)
+        Dim ConvertedBase64Image As Image = Image.FromStream(ms, True)
+        Return ConvertedBase64Image
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        savefile()
+    End Sub
+
+    Private Sub cmdopen_Click(sender As Object, e As EventArgs) Handles cmdopen.Click
+        dgv1.Rows.Clear()
+        dgv1.Rows.Add(1)
+
+        Using sr As StreamReader = New StreamReader("aa.txt")
+
+            Dim g As Integer = 0
+            Dim li As String
+            Do Until sr.EndOfStream = True
+                li = sr.ReadLine()
+                dgv1.Rows.Insert(dgv1.CurrentRow.Index, 1)
+                Dim xyz As Array = Split(li, Chr(2))
+                dgv1.Rows(g).Cells("VisitorName").Value = xyz(0)
+                dgv1.Rows(g).Cells("Age").Value = xyz(1)
+                dgv1.Rows(g).Cells("Address").Value = xyz(2)
+                dgv1.Rows(g).Cells("VisitorImage").Value = Base64ToImage(xyz(3))
+                dgv1.Rows(g).Cells("VisitordateAndTime").Value = xyz(4)
+                dgv1.Rows(g).Cells("OutTime").Value = xyz(5)
+                g = g + 1
+            Loop
+            sr.Close()
+        End Using
+    End Sub
+
+    Private Sub cmdSetOutTime_Click(sender As Object, e As EventArgs) Handles cmdSetOutTime.Click
+        dgv1.CurrentRow.Cells("OutTime").Value = Date.Now
+    End Sub
+
+    Private Sub txtSeacrh_TextChanged(sender As Object, e As EventArgs) Handles txtSeacrh.TextChanged
+        saerch()
+    End Sub
+    Sub saerch()
+        With dgv1
+            For i As Integer = 0 To .RowCount - 2
+                Dim aa As String = ""
+                For j As Integer = 1 To 5
+                    aa = aa & .Rows(i).Cells(j).Value.ToString
+                Next
+                If aa.ToLower.Contains(txtSeacrh.Text.ToLower) Then
+                    .Rows(i).Visible = True
+                Else
+                    .Rows(i).Visible = False
+                End If
+
+            Next
+        End With
+    End Sub
+
+    Private Sub cmdClearSearch_Click(sender As Object, e As EventArgs) Handles cmdClearSearch.Click
+        txtSeacrh.Text = ""
+    End Sub
+End Class
