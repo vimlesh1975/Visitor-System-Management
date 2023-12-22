2aaa05f555d9b2b24cf16f8bd7a4045929a4ff40
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-12-22 13:13:12 +0530

    All

--- WebCam/Form1.Designer.vb
+++ WebCam/Form1.Designer.vb
@@ -24,11 +24,6 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.dgv1 = New System.Windows.Forms.DataGridView()
-        Me.VisitorImage = New System.Windows.Forms.DataGridViewImageColumn()
-        Me.VisitorName = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Age = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Address = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.VisitordateAndTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.cmdno = New System.Windows.Forms.Button()
         Me.cmdok = New System.Windows.Forms.Button()
         Me.pbcapture = New PictureBoxAbouAmmar.صورة_دائرة()
@@ -39,6 +34,19 @@ Partial Class Form1
         Me.cmdPrintSelected = New System.Windows.Forms.Button()
         Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
         Me.cmdPrintPreview = New System.Windows.Forms.Button()
+        Me.SN = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VisitorImage = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.VisitorName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Age = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Address = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MobileNumber = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VisitordateAndTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.OutTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.OfficerToMeet = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NatureOfWork = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.IssuingOfficer = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Reference = New System.Windows.Forms.DataGridViewComboBoxColumn()
+        Me.TotalPerson = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.dgv1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.pbcapture, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.pbcaptureimage, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -48,12 +56,110 @@ Partial Class Form1
         '
         Me.dgv1.AllowUserToAddRows = False
         Me.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgv1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.VisitorImage, Me.VisitorName, Me.Age, Me.Address, Me.VisitordateAndTime})
-        Me.dgv1.Location = New System.Drawing.Point(12, 46)
+        Me.dgv1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.SN, Me.VisitorImage, Me.VisitorName, Me.Age, Me.Address, Me.MobileNumber, Me.VisitordateAndTime, Me.OutTime, Me.OfficerToMeet, Me.NatureOfWork, Me.IssuingOfficer, Me.Reference, Me.TotalPerson})
+        Me.dgv1.Location = New System.Drawing.Point(3, 419)
         Me.dgv1.Name = "dgv1"
         Me.dgv1.RowTemplate.Height = 50
-        Me.dgv1.Size = New System.Drawing.Size(864, 717)
+        Me.dgv1.Size = New System.Drawing.Size(1859, 363)
         Me.dgv1.TabIndex = 4
+        '
+        'cmdno
+        '
+        Me.cmdno.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdno.Image = Global.WebCam.My.Resources.Resources.Actions_edit_delete_icon
+        Me.cmdno.Location = New System.Drawing.Point(717, 345)
+        Me.cmdno.Name = "cmdno"
+        Me.cmdno.Size = New System.Drawing.Size(32, 24)
+        Me.cmdno.TabIndex = 3
+        Me.cmdno.UseVisualStyleBackColor = True
+        '
+        'cmdok
+        '
+        Me.cmdok.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.cmdok.Image = Global.WebCam.My.Resources.Resources.Ok_icon__1_1
+        Me.cmdok.Location = New System.Drawing.Point(679, 345)
+        Me.cmdok.Name = "cmdok"
+        Me.cmdok.Size = New System.Drawing.Size(32, 24)
+        Me.cmdok.TabIndex = 2
+        Me.cmdok.UseVisualStyleBackColor = True
+        '
+        'pbcapture
+        '
+        Me.pbcapture.BackColor = System.Drawing.Color.DarkGray
+        Me.pbcapture.BackgroundImage = Global.WebCam.My.Resources.Resources.Image_Capture_icon
+        Me.pbcapture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbcapture.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.pbcapture.Location = New System.Drawing.Point(686, 329)
+        Me.pbcapture.Name = "pbcapture"
+        Me.pbcapture.Size = New System.Drawing.Size(52, 50)
+        Me.pbcapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pbcapture.TabIndex = 1
+        Me.pbcapture.TabStop = False
+        '
+        'pbcaptureimage
+        '
+        Me.pbcaptureimage.Location = New System.Drawing.Point(440, 8)
+        Me.pbcaptureimage.Name = "pbcaptureimage"
+        Me.pbcaptureimage.Size = New System.Drawing.Size(487, 313)
+        Me.pbcaptureimage.TabIndex = 0
+        Me.pbcaptureimage.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(91, 385)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cmdopen
+        '
+        Me.cmdopen.Location = New System.Drawing.Point(10, 385)
+        Me.cmdopen.Name = "cmdopen"
+        Me.cmdopen.Size = New System.Drawing.Size(75, 23)
+        Me.cmdopen.TabIndex = 6
+        Me.cmdopen.Text = "Open"
+        Me.cmdopen.UseVisualStyleBackColor = True
+        '
+        'PrintDocument1
+        '
+        '
+        'cmdPrintSelected
+        '
+        Me.cmdPrintSelected.Location = New System.Drawing.Point(332, 390)
+        Me.cmdPrintSelected.Name = "cmdPrintSelected"
+        Me.cmdPrintSelected.Size = New System.Drawing.Size(90, 23)
+        Me.cmdPrintSelected.TabIndex = 7
+        Me.cmdPrintSelected.Text = "Print"
+        Me.cmdPrintSelected.UseVisualStyleBackColor = True
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'cmdPrintPreview
+        '
+        Me.cmdPrintPreview.Location = New System.Drawing.Point(216, 390)
+        Me.cmdPrintPreview.Name = "cmdPrintPreview"
+        Me.cmdPrintPreview.Size = New System.Drawing.Size(110, 23)
+        Me.cmdPrintPreview.TabIndex = 8
+        Me.cmdPrintPreview.Text = "Print Preview"
+        Me.cmdPrintPreview.UseVisualStyleBackColor = True
+        '
+        'SN
+        '
+        Me.SN.HeaderText = "SN"
+        Me.SN.Name = "SN"
+        Me.SN.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.SN.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        Me.SN.Width = 40
         '
         'VisitorImage
         '
@@ -79,107 +185,59 @@ Partial Class Form1
         Me.Address.Name = "Address"
         Me.Address.Width = 300
         '
+        'MobileNumber
+        '
+        Me.MobileNumber.HeaderText = "Mobile Number"
+        Me.MobileNumber.Name = "MobileNumber"
+        Me.MobileNumber.Width = 120
+        '
         'VisitordateAndTime
         '
         Me.VisitordateAndTime.HeaderText = "Time"
         Me.VisitordateAndTime.Name = "VisitordateAndTime"
         Me.VisitordateAndTime.Width = 150
         '
-        'cmdno
+        'OutTime
         '
-        Me.cmdno.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.cmdno.Image = Global.WebCam.My.Resources.Resources.Actions_edit_delete_icon
-        Me.cmdno.Location = New System.Drawing.Point(1159, 380)
-        Me.cmdno.Name = "cmdno"
-        Me.cmdno.Size = New System.Drawing.Size(32, 24)
-        Me.cmdno.TabIndex = 3
-        Me.cmdno.UseVisualStyleBackColor = True
+        Me.OutTime.HeaderText = "OutTime"
+        Me.OutTime.Name = "OutTime"
+        Me.OutTime.Width = 150
         '
-        'cmdok
+        'OfficerToMeet
         '
-        Me.cmdok.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.cmdok.Image = Global.WebCam.My.Resources.Resources.Ok_icon__1_1
-        Me.cmdok.Location = New System.Drawing.Point(1121, 380)
-        Me.cmdok.Name = "cmdok"
-        Me.cmdok.Size = New System.Drawing.Size(32, 24)
-        Me.cmdok.TabIndex = 2
-        Me.cmdok.UseVisualStyleBackColor = True
+        Me.OfficerToMeet.HeaderText = "OfficerToMeet"
+        Me.OfficerToMeet.Name = "OfficerToMeet"
+        Me.OfficerToMeet.Width = 200
         '
-        'pbcapture
+        'NatureOfWork
         '
-        Me.pbcapture.BackColor = System.Drawing.Color.DarkGray
-        Me.pbcapture.BackgroundImage = Global.WebCam.My.Resources.Resources.Image_Capture_icon
-        Me.pbcapture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.pbcapture.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.pbcapture.Location = New System.Drawing.Point(1128, 364)
-        Me.pbcapture.Name = "pbcapture"
-        Me.pbcapture.Size = New System.Drawing.Size(52, 50)
-        Me.pbcapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.pbcapture.TabIndex = 1
-        Me.pbcapture.TabStop = False
+        Me.NatureOfWork.HeaderText = "Nature Of Work"
+        Me.NatureOfWork.Name = "NatureOfWork"
+        Me.NatureOfWork.Width = 150
         '
-        'pbcaptureimage
+        'IssuingOfficer
         '
-        Me.pbcaptureimage.Location = New System.Drawing.Point(882, 43)
-        Me.pbcaptureimage.Name = "pbcaptureimage"
-        Me.pbcaptureimage.Size = New System.Drawing.Size(487, 313)
-        Me.pbcaptureimage.TabIndex = 0
-        Me.pbcaptureimage.TabStop = False
+        Me.IssuingOfficer.HeaderText = "Issuing Officer"
+        Me.IssuingOfficer.Name = "IssuingOfficer"
+        Me.IssuingOfficer.Width = 150
         '
-        'Button1
+        'Reference
         '
-        Me.Button1.Location = New System.Drawing.Point(95, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 5
-        Me.Button1.Text = "Save"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Reference.HeaderText = "Reference"
+        Me.Reference.Items.AddRange(New Object() {"Rehersal", "Recording", "Live Telecast", "Personal Meet", "Reference"})
+        Me.Reference.Name = "Reference"
         '
-        'cmdopen
+        'TotalPerson
         '
-        Me.cmdopen.Location = New System.Drawing.Point(14, 12)
-        Me.cmdopen.Name = "cmdopen"
-        Me.cmdopen.Size = New System.Drawing.Size(75, 23)
-        Me.cmdopen.TabIndex = 6
-        Me.cmdopen.Text = "Open"
-        Me.cmdopen.UseVisualStyleBackColor = True
-        '
-        'PrintDocument1
-        '
-        '
-        'cmdPrintSelected
-        '
-        Me.cmdPrintSelected.Location = New System.Drawing.Point(336, 17)
-        Me.cmdPrintSelected.Name = "cmdPrintSelected"
-        Me.cmdPrintSelected.Size = New System.Drawing.Size(90, 23)
-        Me.cmdPrintSelected.TabIndex = 7
-        Me.cmdPrintSelected.Text = "Print"
-        Me.cmdPrintSelected.UseVisualStyleBackColor = True
-        '
-        'PrintPreviewDialog1
-        '
-        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
-        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
-        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
-        Me.PrintPreviewDialog1.Enabled = True
-        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
-        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
-        Me.PrintPreviewDialog1.Visible = False
-        '
-        'cmdPrintPreview
-        '
-        Me.cmdPrintPreview.Location = New System.Drawing.Point(220, 17)
-        Me.cmdPrintPreview.Name = "cmdPrintPreview"
-        Me.cmdPrintPreview.Size = New System.Drawing.Size(110, 23)
-        Me.cmdPrintPreview.TabIndex = 8
-        Me.cmdPrintPreview.Text = "Print Preview"
-        Me.cmdPrintPreview.UseVisualStyleBackColor = True
+        Me.TotalPerson.HeaderText = "Total Person"
+        Me.TotalPerson.Name = "TotalPerson"
+        Me.TotalPerson.Width = 90
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1385, 793)
+        Me.ClientSize = New System.Drawing.Size(1866, 793)
         Me.Controls.Add(Me.cmdPrintPreview)
         Me.Controls.Add(Me.cmdPrintSelected)
         Me.Controls.Add(Me.cmdopen)
@@ -204,13 +262,21 @@ Partial Class Form1
     Friend WithEvents dgv1 As DataGridView
     Friend WithEvents Button1 As Button
     Friend WithEvents cmdopen As Button
-    Friend WithEvents VisitorImage As DataGridViewImageColumn
-    Friend WithEvents VisitorName As DataGridViewTextBoxColumn
-    Friend WithEvents Age As DataGridViewTextBoxColumn
-    Friend WithEvents Address As DataGridViewTextBoxColumn
-    Friend WithEvents VisitordateAndTime As DataGridViewTextBoxColumn
     Friend WithEvents PrintDocument1 As Printing.PrintDocument
     Friend WithEvents cmdPrintSelected As Button
     Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
     Friend WithEvents cmdPrintPreview As Button
+    Friend WithEvents SN As DataGridViewTextBoxColumn
+    Friend WithEvents VisitorImage As DataGridViewImageColumn
+    Friend WithEvents VisitorName As DataGridViewTextBoxColumn
+    Friend WithEvents Age As DataGridViewTextBoxColumn
+    Friend WithEvents Address As DataGridViewTextBoxColumn
+    Friend WithEvents MobileNumber As DataGridViewTextBoxColumn
+    Friend WithEvents VisitordateAndTime As DataGridViewTextBoxColumn
+    Friend WithEvents OutTime As DataGridViewTextBoxColumn
+    Friend WithEvents OfficerToMeet As DataGridViewTextBoxColumn
+    Friend WithEvents NatureOfWork As DataGridViewTextBoxColumn
+    Friend WithEvents IssuingOfficer As DataGridViewTextBoxColumn
+    Friend WithEvents Reference As DataGridViewComboBoxColumn
+    Friend WithEvents TotalPerson As DataGridViewTextBoxColumn
 End Class
--- WebCam/Form1.vb
+++ WebCam/Form1.vb
@@ -24,19 +24,24 @@ Public Class Form1
         cmdok.Visible = False
         dgv1.Rows.Add(1)
         Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
-        ' CAMARAS.VideoDevice.Source = "USB2.0 PC CAMERA"
+
+
+
         If CAMARAS.ShowDialog() = DialogResult.OK Then
             CAMARA = CAMARAS.VideoDevice
-
-            'CAMARA.Source = "USB2.0 PC CAMERA"
+            CAMARA.Source = "@device:pnp:\\?\usb#vid_1908&pid_2311&mi_00#6&203b7b69&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global"
             AddHandler CAMARA.NewFrame, New NewFrameEventHandler(AddressOf CAPTURAR)
             CAMARA.Start()
             cameraStarted = True
         Else
             Me.Close()
         End If
+
+
         opeFile()
-        dgv1.Rows(1).Selected = True
+        dgv1.CurrentCell = dgv1(0, 1)
+
+
     End Sub
     Private Sub CAPTURAR(sender As Object, eventArgs As NewFrameEventArgs)
 
@@ -106,6 +111,16 @@ Public Class Form1
         dgv1.CurrentRow.Cells("Address").Value = "Pratapgad 505, kamothe, Navi Mumbai, mh 410209"
         dgv1.CurrentRow.Cells("VisitordateAndTime").Value = Date.Now
 
+        dgv1.CurrentRow.Cells("SN").Value = "0"
+        dgv1.CurrentRow.Cells("MobileNumber").Value = "0000000000"
+        dgv1.CurrentRow.Cells("OutTime").Value = Date.Now.AddHours(2)
+        dgv1.CurrentRow.Cells("OfficerToMeet").Value = "OfficerToMeet"
+        dgv1.CurrentRow.Cells("NatureOfWork").Value = "NatureOfWork"
+        dgv1.CurrentRow.Cells("IssuingOfficer").Value = "IssuingOfficer"
+        dgv1.CurrentRow.Cells("Reference").Value = "Reference"
+        dgv1.CurrentRow.Cells("TotalPerson").Value = "TotalPerson"
+
+
 
         Dim CAMARAS As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
         CAMARA.Start()
@@ -154,7 +169,20 @@ Public Class Form1
                     'If dgv1.Rows(f).Cells("Conversion").Value = False Then dgv1.Rows(f).Cells("Conversion").Value = "0"
                     'If dgv1.Rows(f).Cells("BackIn").Value = False Then dgv1.Rows(f).Cells("BackIn").Value = "0"
 
-                    sw.WriteLine(dgv1.Rows(f).Cells("VisitorName").Value & Chr(2) & dgv1.Rows(f).Cells("Age").Value & Chr(2) & dgv1.Rows(f).Cells("Address").Value & Chr(2) & ImageToBase64(dgv1.Rows(f).Cells("VisitorImage").Value, Imaging.ImageFormat.Png) & Chr(2) & dgv1.Rows(f).Cells("VisitordateAndTime").Value)
+                    sw.WriteLine(dgv1.Rows(f).Cells("VisitorName").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("Age").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("Address").Value _
+                       & Chr(2) & ImageToBase64(dgv1.Rows(f).Cells("VisitorImage").Value, Imaging.ImageFormat.Png) _
+                       & Chr(2) & dgv1.Rows(f).Cells("VisitordateAndTime").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("SN").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("MobileNumber").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("OutTime").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("OfficerToMeet").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("NatureOfWork").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("IssuingOfficer").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("Reference").Value _
+                       & Chr(2) & dgv1.Rows(f).Cells("TotalPerson").Value
+                        )
                     f = f + 1
                 Loop
             End If
@@ -164,9 +192,39 @@ Public Class Form1
         'SaveToolStripMenuItem.Enabled = True
         'End If
     End Sub
+    Sub opeFile()
+        On Error Resume Next
+        Using sr As StreamReader = New StreamReader("aa.txt")
+            Dim g As Integer = 0
+            Dim li As String
+            Do Until sr.EndOfStream = True
+                li = sr.ReadLine()
+                dgv1.Rows.Insert(dgv1.CurrentRow.Index, 1)
+                Dim xyz As Array = Split(li, Chr(2))
+                dgv1.Rows(g).Cells("VisitorName").Value = xyz(0)
+                dgv1.Rows(g).Cells("Age").Value = xyz(1)
+                dgv1.Rows(g).Cells("Address").Value = xyz(2)
+                dgv1.Rows(g).Cells("VisitorImage").Value = Base64ToImage(xyz(3))
+                dgv1.Rows(g).Cells("VisitordateAndTime").Value = xyz(4)
 
+                dgv1.Rows(g).Cells("SN").Value = xyz(5)
+                dgv1.Rows(g).Cells("MobileNumber").Value = xyz(6)
+                dgv1.Rows(g).Cells("OutTime").Value = xyz(7)
+                dgv1.Rows(g).Cells("OfficerToMeet").Value = xyz(8)
+                dgv1.Rows(g).Cells("NatureOfWork").Value = xyz(9)
+                dgv1.Rows(g).Cells("IssuingOfficer").Value = xyz(10)
+                dgv1.Rows(g).Cells("Reference").Value = xyz(11)
+                dgv1.Rows(g).Cells("TotalPerson").Value = xyz(12)
+
+
+
+                g = g + 1
+            Loop
+            sr.Close()
+        End Using
+    End Sub
     Private Sub dgv1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv1.CellContentClick
-        If e.ColumnIndex = 0 Then
+        If e.ColumnIndex = 1 Then
             Form2.Show()
             Form2.pbcaptureimage.Image = dgv1.CurrentRow.Cells("VisitorImage").Value
         End If
@@ -205,23 +263,22 @@ Public Class Form1
         opeFile()
     End Sub
 
-    Sub opeFile()
-        Using sr As StreamReader = New StreamReader("aa.txt")
-            Dim g As Integer = 0
-            Dim li As String
-            Do Until sr.EndOfStream = True
-                li = sr.ReadLine()
-                dgv1.Rows.Insert(dgv1.CurrentRow.Index, 1)
-                Dim xyz As Array = Split(li, Chr(2))
-                dgv1.Rows(g).Cells("VisitorName").Value = xyz(0)
-                dgv1.Rows(g).Cells("Age").Value = xyz(1)
-                dgv1.Rows(g).Cells("Address").Value = xyz(2)
-                dgv1.Rows(g).Cells("VisitorImage").Value = Base64ToImage(xyz(3))
-                dgv1.Rows(g).Cells("VisitordateAndTime").Value = xyz(4)
-                g = g + 1
-            Loop
-            sr.Close()
-        End Using
+    Sub drawRectangle(e, rectangleX, rectangleY, rectangleWidth, rectangleHeight)
+        ' Assume you have a Graphics object, for example, e.Graphics in a Paint event
+        Dim graphics As Graphics = e.Graphics
+
+        ' Specify the rectangle's properties
+        Dim rectanglePen As New Pen(Color.Black) ' Pen to define the rectangle's border color
+        'Dim rectangleX As Integer = 400 ' X-coordinate of the top-left corner of the rectangle
+        'Dim rectangleY As Integer = 220 ' Y-coordinate of the top-left corner of the rectangle
+        'Dim rectangleWidth As Integer = 200 ' Width of the rectangle
+        'Dim rectangleHeight As Integer = 50 ' Height of the rectangle
+
+        ' Draw the rectangle
+        graphics.DrawRectangle(rectanglePen, rectangleX, rectangleY, rectangleWidth, rectangleHeight)
+
+        ' Dispose of the Pen to free up resources when done
+        rectanglePen.Dispose()
     End Sub
 
     Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
@@ -229,18 +286,39 @@ Public Class Form1
         Dim scale As Single = 0.25
         Dim newWidth As Single = image.Width * scale
         Dim newHeight As Single = image.Height * scale
-        Dim destinationRect As New RectangleF(100, 100, newWidth, newHeight)
+        Dim destinationRect As New RectangleF(650, 100, newWidth, newHeight)
         e.Graphics.DrawImage(image, destinationRect)
 
         Dim printFont As New Font("Arial", 12)
         Dim printBrush As New SolidBrush(Color.Black)
         Dim textToPrint As String =
-         "Name:" + dgv1.CurrentRow.Cells("VisitorName").Value + vbNewLine _
-       + "Age: " + dgv1.CurrentRow.Cells("Age").Value + vbNewLine _
-       + "Address: " + dgv1.CurrentRow.Cells("Address").Value + vbNewLine _
-       + "Time: " + dgv1.CurrentRow.Cells("VisitordateAndTime").Value + vbNewLine
+            "Sr. No." + vbNewLine _
+          & "DOORDARSHAN KENDRA, MUMBAI" + vbNewLine _
+          & "Worli, Mumbai - 400030" + vbNewLine _
+           & "_________________________________________" + vbNewLine _
+            & "Reference:" + dgv1.CurrentRow.Cells("Reference").Value + vbNewLine _
+            & "Mobile Number:" + dgv1.CurrentRow.Cells("MobileNumber").Value + vbNewLine _
+            & "Total Number:" + dgv1.CurrentRow.Cells("TotalPerson").Value + vbNewLine _
+            & "Signature of Visitor:" + vbNewLine _
+        & "Name:" + dgv1.CurrentRow.Cells("VisitorName").Value + vbNewLine _
+       & "Age: " + (dgv1.CurrentRow.Cells("Age").Value).ToString + vbNewLine _
+       & "Address: " + dgv1.CurrentRow.Cells("Address").Value + vbNewLine _
+       & "Visiting Officer's name: " + dgv1.CurrentRow.Cells("OfficerToMeet").Value + vbNewLine _
+       & "Nature of Work: " + dgv1.CurrentRow.Cells("NatureOfWork").Value + vbNewLine _
+       & "In  Time: " + dgv1.CurrentRow.Cells("VisitordateAndTime").Value + vbNewLine _
+       & "Out Time: " + dgv1.CurrentRow.Cells("OutTime").Value + vbNewLine _
+       & "Issuing Officer's Signature: " + dgv1.CurrentRow.Cells("IssuingOfficer").Value + vbNewLine _
+       + vbNewLine _
+       & "Visited Officer's Name and Signature: " + vbNewLine _
+       + vbNewLine _
+        + vbNewLine _
+       & "(Please keep this paper with you and return to Main Gate at the time of leaving Dooradrshan)"
 
-        e.Graphics.DrawString(textToPrint, printFont, printBrush, 270, 100)
+        e.Graphics.DrawString(textToPrint, printFont, printBrush, 100, 100)
+
+        drawRectangle(e, 400, 220, 200, 50)
+        drawRectangle(e, 600, 370, 200, 50)
+        drawRectangle(e, 600, 425, 200, 50)
 
     End Sub
 
